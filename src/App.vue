2fe6eb0ff0a9ab02fb8847924435265d233562d5
Timothy Partaliano <timothypartaliano@gmail.com>
2024-02-01 11:16:56 +0700

    setup vue + vite

---
+++ src/App.vue
@@ -0,0 +1,25 @@
+<script>
+  export default {
+    data() {
+      return {
+        title: 'Hello World',
+        subtitle: 'Hello Vue'
+      }
+    }
+  }
+</script>
+
+<template>
+  <h1>{{ title }}</h1>
+    <br>
+  <h2>{{ subtitle }}</h2>
+</template>
+
+<style>
+  h1 {
+    color: blue;
+  }
+  h2 {
+    color: aqua;
+  }
+</style>
