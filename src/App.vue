a973fc115d39948e381a0d5c43370c4632b10ed3
Timothy Partaliano <timothypartaliano@gmail.com>
2024-02-13 10:11:00 +0700

    add props

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,10 @@
 <script>
-  export default {
+import CounterApp from './components/CounterApp.vue'
+
+export default {
     data() {
       return {
         number: 0,
-        input: 0,
         input: 0
       }
     },
@@ -13,13 +14,14 @@
         this.input = 0
         event.preventDefault()
     }
-  }
+  },
+  components: { CounterApp }
 }
 </script>
 
 <template>
   <h1>Counter App</h1>
-  <h2>{{ number }}</h2>
+  <CounterApp :number="number" title="Component App"></CounterApp>
   
   <form @submit="increment">
     <input type="number" v-model="input">
---
+++ src/components/CounterApp.vue
@@ -0,0 +1,13 @@
+<script>
+export default {
+    props: {
+        title: String,
+        number: Number
+    }
+}
+</script>
+
+<template>
+    <h1>{{ title }}</h1>
+    <h1>{{ number }}</h1>
+</template>
